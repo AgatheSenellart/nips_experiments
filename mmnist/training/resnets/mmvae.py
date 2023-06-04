34e27464f0d2456b5a7aaa0a0c6006fd9f97a927
Agathe <agathe.senellart@polytechnique.edu>
2023-06-04 20:27:15 +0200

    mmvae out of memory

--- mmnist/training/resnets/mmvae.py
+++ mmnist/training/resnets/mmvae.py
@@ -42,7 +42,7 @@ trainer_config = BaseTrainerConfig(
     seed=args.seed,
     output_dir=f"compare_on_mmnist/{config_name}/{model.model_name}/seed_{args.seed}/missing_ratio_{args.missing_ratio}/K__{model.K}",
 )
-trainer_config.per_device_train_batch_size = 128
+trainer_config.per_device_train_batch_size = 64
 trainer_config.num_epochs = 50 # enough for this model to reach convergence
 
 # Set up callbacks
